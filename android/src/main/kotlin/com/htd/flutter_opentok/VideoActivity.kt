e85c1894a176279ad51187199ab5d472581998f5
kamilklyta <kamil.klyta@htdevelopers.com>
2019-08-12 17:40:58 +0200

    remove unused import

--- android/src/main/kotlin/com/htd/flutter_opentok/VideoActivity.kt
+++ android/src/main/kotlin/com/htd/flutter_opentok/VideoActivity.kt
@@ -9,7 +9,6 @@ import android.app.Activity
  import android.view.View
  import android.widget.Button
  import android.widget.FrameLayout
- import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 
 import com.opentok.android.Session
